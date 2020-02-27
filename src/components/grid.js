6daab967eec2fd183abd0d946b69cf7ae9436ff1
ayeh0418 <andrewyeh0418@gmail.com>
2020-02-27 07:04:35 -0800

    can remove dots

--- src/components/grid.js
+++ src/components/grid.js
@@ -3,6 +3,7 @@ import "./grid.css";
 import dotPic from '../images/dot.png';
 
 var idx = 0;
+var onDot = false;
 
 export default class grid extends Component {  
 	constructor(props) {
@@ -11,30 +12,47 @@ export default class grid extends Component {
 		this.state = {
 			dots: []
 		};
+		this.removeDot = this.removeDot.bind(this);
+		this._onClick = this._onClick.bind(this);
 	}
 
 	_onClick(e) {
-		var bounds = e.target.getBoundingClientRect();
-		var xCoord = e.screenX - bounds.left - 15;
-		var yCoord = e.screenY - bounds.top - 85;
-		this.setState({
-			dots: this.state.dots.concat([
-				{
-					x: xCoord, 
-					y: yCoord
-				}
-			])
-		});
+		if (!onDot) {
+			var bounds = e.target.getBoundingClientRect();
+			var xCoord = e.screenX - bounds.left - 15;
+			var yCoord = e.screenY - bounds.top - 85;
+			this.setState({
+				dots: this.state.dots.concat([
+					{
+						id: idx,
+						x: xCoord, 
+						y: yCoord
+					}
+				])
+			});
+			idx += 1;
+		} else {
+			onDot = false;
+		}
+	}
+
+	removeDot(id) {
+		onDot = true;
+		const newState = this.state;
+		const index = newState.dots.findIndex(a => a.id === id);
+		if (index === -1) return;
+		newState.dots.splice(index, 1);
+		this.setState(newState);
 	}
 
 	render() {
-		const dots = this.state.dots.map(function(data, i){
-			return <img src={dotPic} alt="dot" 
+		const dots = this.state.dots.map((data, i) => {
+			return <img onClick={() => this.removeDot(data.id)} src={dotPic} alt="dot" 
 					style={{width: 30, height: 30, position: 'absolute', top: data.y, left: data.x}}/>;
 		});
 		return (
 			<div>
-				<div onClick={this._onClick.bind(this)} className="board">
+				<div onClick={this._onClick} className="board">
 					{dots}
 				</div> 
 			</div>
