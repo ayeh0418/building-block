9731f9b4bec1af1bd4aac62e6b910024dfd0a87d
ArthurTran <art017@ucsd.edu>
2020-02-27 06:02:15 -0800

    facebook

--- src/components/Facebook.js
+++ src/components/Facebook.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import FacebookLogin from 'react-facebook-login';
+import fire from '../fire';
+import { Link, Redirect } from 'react-router-dom';
 
 export default class Facebook extends Component {
 	state = {
@@ -7,25 +9,65 @@ export default class Facebook extends Component {
 		userID: '',
 		name: '',
 		email: '',
-		picutre: ''
+		picture: '',
+		redirect: false
 	}
 
 	responseFacebook = response => {
 		this.setState({
-			isLoggedIn: true,
+			isLoggedIn: false,
 			userID: response.userID,
 			name: response.name,
 			email: response.email,
 			picture: response.picture.data.url
 		});
-	}
 
+		var redirect = false;
+		var fblogin = false;
+		var ref = fire.database().ref('users');
+		var userEmail = this.state.email.replace(/\./g, ',');
+
+		ref.on("value", function(snapshot) {
+			if(snapshot.hasChild(userEmail)){
+				console.log("good");
+				redirect = true;
+				console.log(redirect);
+				fblogin = true;
+				console.log(fblogin);
+			}
+			else{
+				console.log("bad");
+				redirect = false;
+			}
+			
+		}, function (errorObject) {
+			console.log("The read failed: " + errorObject.code);
+			redirect = false;
+			alert('wrong info');
+		});
+
+		console.log(fblogin);
+		if(fblogin){
+			this.setState({isLoggedIn: true });
+			console.log(this.state.isLoggedIn);
+		}
+
+		
+	}
+	
 	componentClicked = () => console.log('clicked');
 
 	render () {
+
+
+		/*else if(userExist == false){
+			//return <Redirect push to="/Signup" />;
+		}
+		*/
 		let fbContent;
 
 		if (this.state.isLoggedIn) {
+			return <Redirect push to="/Home" />;
 			fbContent = (
 				<div style={{
 					margin: 'auto',
@@ -35,18 +77,22 @@ export default class Facebook extends Component {
 					<img src={this.state.picture} alt={this.state.name} />
 					<h2>{this.state.name}</h2>
 				</div>
+				
 			)
 		} else {
 			fbContent = (
 				 <FacebookLogin
 				appId="1041187736251143"
-				autoLoad={true}
+				autoLoad={false}
 				fields="name,email,picture"
 				onClick={this.componentClicked}
 				callback={this.responseFacebook} />
 			);
 		}
 
+		if(this.state.redirect == true){
+				return <Redirect push to="/Home" />;
+		}
 		return (
 			<div>{fbContent}</div>
 		);
