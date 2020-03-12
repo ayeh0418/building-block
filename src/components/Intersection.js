caf305e7e20d2be434ff7cfed69f43cc2c734a44
art017 <art017@ucsd.edu>
2020-03-12 08:48:29 -0700

    intersection

--- src/components/Intersection.js
+++ src/components/Intersection.js
@@ -8,26 +8,42 @@ export default class Intersection extends Component {
 		super(prop, context);
 		
 		this.state = {
-			showDot: false
+			showDot: false,
+			dotNum: ''
 		};
 
 		this.handleDot = this.handleDot.bind(this);	
 	}
 
 	handleDot() {
-		this.setState(prevState => ({
-			showDot: !prevState.showDot
-		}));
+		var label = localStorage.getItem("toggleLabel");
+		var number = localStorage.getItem("labelNumber");
+		console.log(label);
+		console.log(number);
+		if(label != 1){
+			this.setState(prevState => ({
+				showDot: !prevState.showDot
+			}));
 
 		
-		if (!this.state.showDot) {
-			var dotRef = fire.database().ref().child("formations");
-			dotRef.child(this.props.index).set({
-				x: this.props.x,
-				y: this.props.y
-			});
-		} else {
-			fire.database().ref("formations/" + this.props.index).remove();
+			if (!this.state.showDot) {
+				var dotRef = fire.database().ref().child("formations");
+				dotRef.child(this.props.index).set({
+					x: this.props.x,
+					y: this.props.y
+				});
+			} else {
+				fire.database().ref("formations/" + this.props.index).remove();
+				if(this.state.dotNum != ''){
+					this.setState({dotNum: ''});
+				}
+			}
+		}
+		else{
+
+			this.setState({dotNum: number});
+			localStorage.setItem('toggleLabel', 0);
+			localStorage.setItem('labelNumber', '');
 		}
 	}
 
@@ -35,7 +51,7 @@ export default class Intersection extends Component {
 		return (
 			<div className="intersection" id={this.props.x + "-" + this.props.y}>
 				<img onClick={this.handleDot} src={cross}/>
-				<div onClick={this.handleDot} className="black" style={{visibility: this.state.showDot ? 'visible' : 'hidden' }}></div>
+				<div onClick={this.handleDot} className="black" style={{visibility: this.state.showDot ? 'visible' : 'hidden' }}>{this.state.dotNum}</div>
 			</div>
 		);
 	}
